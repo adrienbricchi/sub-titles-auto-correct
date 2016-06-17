d27311aa8a8f586b1ae6176e48626679cb85e90d
Adrien Bricchi <adrienbricchi@users.noreply.github.com>
2016-06-17 21:57:07 +0200

    Fix colon unittest

--- Corrector/Tests/test_StringsUtils.py
+++ Corrector/Tests/test_StringsUtils.py
@@ -8,111 +8,103 @@ from Corrector.Utils import Consts
 
 Consts.is_unittest_exec = True
 
-# noinspection PyDictCreation
 TEST_LINES = {}
-# noinspection PyDictCreation
 RESULT_LINES = {}
+RESULT_PROMPT = {}
 
-# region TEST_LINES Single
-
-TEST_LINES["fix_punctuation_spaces"] = ["Hey! ?What ? ! ? !!\n", "Ok ! \"Line 2?\"?\n"]
-RESULT_LINES["fix_punctuation_spaces"] = ["Hey !? What ?!?!!\n", "Ok ! \"Line 2 ?\" ?\n"]
-
-TEST_LINES["fix_common_misspells_fr"] = ["Seinfelf. II. Evidemment\n"]
-RESULT_LINES["fix_common_misspells_fr"] = ["Seinfeld. Il. Évidemment\n"]
-TEST_LINES["fix_common_misspells_eng"] = ["Seinfelf. II yourjob\n"]
-RESULT_LINES["fix_common_misspells_eng"] = ["Seinfeld. II your job\n"]
-TEST_LINES["fix_common_misspells"] = ["Seinfelf lran 9 mm\n"]
-RESULT_LINES["fix_common_misspells"] = ["Seinfeld Iran 9mm\n"]
-
-TEST_LINES["fix_numbers"] = ["Line 333 4 45, 50 and 3, 4, 5\n", "4 ème et 5 h 30 à 20 % et 5 .\n"]
-RESULT_LINES["fix_numbers"] = ["Line 333 445,50 and 3, 4, 5\n", "4ème et 5h30 à 20% et 5.\n"]
-FIX_NUMBERS_PROMPTS = [True, False, False]
-
-TEST_LINES["fix_degree_symbol"] = ["n°1 and n° 2 and N ° 3 and n °4 and 5 °F and 6° F and 7 ° C\n"]
-RESULT_LINES["fix_degree_symbol"] = ["n°1 and n°2 and N°3 and n°4 and 5°F and 6°F and 7°C\n"]
 
 # noinspection SpellCheckingInspection
-TEST_LINES["fix_capital_i_to_l"] = ["Il AIbert AI pIop fataI AIIIIb\n"]
-# noinspection SpellCheckingInspection
-RESULT_LINES["fix_capital_i_to_l"] = ["Il Albert AI plop fatal Allllb\n"]
+def populate_single_line_test_dict():
+
+    TEST_LINES["fix_punctuation_spaces"] = ["Hey! ?What ? ! ? !!\n", "Ok ! \"Line 2?\"?\n"]
+    RESULT_LINES["fix_punctuation_spaces"] = ["Hey !? What ?!?!!\n", "Ok ! \"Line 2 ?\" ?\n"]
+
+    TEST_LINES["fix_colon"] = ["TEST: line. Other test:\n", "12: 44 or 12 : 45 or 12 :45 or 12:46 or: 7\n"]
+    RESULT_LINES["fix_colon"] = ["TEST : line. Other test :\n", "12:44 or 12:45 or 12:45 or 12:46 or : 7\n"]
+
+    TEST_LINES["fix_common_misspells_fr"] = ["Seinfelf. II. Evidemment\n"]
+    RESULT_LINES["fix_common_misspells_fr"] = ["Seinfeld. Il. Évidemment\n"]
+    TEST_LINES["fix_common_misspells_eng"] = ["Seinfelf. II yourjob\n"]
+    RESULT_LINES["fix_common_misspells_eng"] = ["Seinfeld. II your job\n"]
+    TEST_LINES["fix_common_misspells"] = ["Seinfelf lran 9 mm\n"]
+    RESULT_LINES["fix_common_misspells"] = ["Seinfeld Iran 9mm\n"]
+
+    TEST_LINES["fix_numbers"] = ["Line 333 4 45, 50 and 3, 4, 5\n", "4 ème et 5 h 30 à 20 % et 5 .\n"]
+    RESULT_LINES["fix_numbers"] = ["Line 333 445,50 and 3, 4, 5\n", "4ème et 5h30 à 20% et 5.\n"]
+    RESULT_PROMPT["fix_numbers"] = [True, False, False]
+
+    TEST_LINES["fix_degree_symbol"] = ["n°1 and n° 2 and N ° 3 and n °4 and 5 °F and 6° F and 7 ° C\n"]
+    RESULT_LINES["fix_degree_symbol"] = ["n°1 and n°2 and N°3 and n°4 and 5°F and 6°F and 7°C\n"]
+
+    TEST_LINES["fix_capital_i_to_l"] = ["Il AIbert AI pIop fataI AIIIIb\n"]
+    RESULT_LINES["fix_capital_i_to_l"] = ["Il Albert AI plop fatal Allllb\n"]
+
+    TEST_LINES["fix_l_to_capital_i"] = ["lnter la test. ln MlB line\n", "Il lou AllB ABll Xlll lll\n"]
+    RESULT_LINES["fix_l_to_capital_i"] = ["Inter la test. In MIB line\n", "Il lou AIIB ABII XIII III\n"]
+
+    TEST_LINES["fix_acronyms"] = ["I. I was here. S. N. C. F. I was\n", "Line 2. I. Line 2. A.T. M. \n"]
+    RESULT_LINES["fix_acronyms"] = ["I. I was here. S.N.C.F. I was\n", "Line 2. I. Line 2. A.T.M. \n"]
+
 
 # noinspection SpellCheckingInspection
-TEST_LINES["fix_l_to_capital_i"] = ["lnter la test. ln MlB line\n", "Il lou AllB ABll Xlll lll\n"]
-# noinspection SpellCheckingInspection
-RESULT_LINES["fix_l_to_capital_i"] = ["Inter la test. In MIB line\n", "Il lou AIIB ABII XIII III\n"]
+def populate_multi_line_test_dict():
 
-TEST_LINES["fix_acronyms"] = ["I. I was here. S. N. C. F. I was\n", "Line 2. I. Line 2. A.T. M. \n"]
-RESULT_LINES["fix_acronyms"] = ["I. I was here. S.N.C.F. I was\n", "Line 2. I. Line 2. A.T.M. \n"]
+    TEST_LINES["fix_redundant_italic_tag"] = ["<i>test</i> <i>line 1</i><i></i>\n", "<i>test</i><i> line 2</i>\n"]
+    RESULT_LINES["fix_redundant_italic_tag"] = ["<i>test line 1\n", "test line 2</i>\n"]
 
-# endregion TEST_LINES Single
+    TEST_LINES["fix_useless_dialog_hyphen_1"] = ["- test line\n"]
+    RESULT_LINES["fix_useless_dialog_hyphen_1"] = ["test line\n"]
+    TEST_LINES["fix_useless_dialog_hyphen_2"] = ["<i>- test line\n"]
+    RESULT_LINES["fix_useless_dialog_hyphen_2"] = ["<i>test line\n"]
+    TEST_LINES["fix_useless_dialog_hyphen_3"] = ["-  test line\n", "test line\n"]
+    RESULT_LINES["fix_useless_dialog_hyphen_3"] = ["test line\n", "test line\n"]
+    TEST_LINES["fix_useless_dialog_hyphen_4"] = ["<i>- test line\n", "test line\n"]
+    RESULT_LINES["fix_useless_dialog_hyphen_4"] = ["<i>test line\n", "test line\n"]
+    TEST_LINES["fix_useless_dialog_hyphen_5"] = ["\"- test line\n", "test line\n"]
+    RESULT_LINES["fix_useless_dialog_hyphen_5"] = ["\"test line\n", "test line\n"]
+    TEST_LINES["fix_useless_dialog_hyphen_6"] = ["\"<i>- test line\n", "test line\n", "test line\n"]
+    RESULT_LINES["fix_useless_dialog_hyphen_6"] = ["\"<i>test line\n", "test line\n", "test line\n"]
 
-# region TEST_LINES Multi
+    TEST_LINES["fix_missing_dialog_hyphen_1"] = ["<i>test line 1</i>\n", "- test line 2\n"]
+    RESULT_LINES["fix_missing_dialog_hyphen_1"] = ["- <i>test line 1</i>\n", "- test line 2\n"]
+    TEST_LINES["fix_missing_dialog_hyphen_2"] = ["<i>test line 1</i>\n", "- test line 2\n", "- test line 3\n"]
+    RESULT_LINES["fix_missing_dialog_hyphen_2"] = ["- <i>test line 1</i>\n", "- test line 2\n", "- test line 3\n"]
+    TEST_LINES["fix_missing_dialog_hyphen_3"] = ["<i>test line 1</i>\n", "test line 2\n", "- test line 3\n"]
+    RESULT_LINES["fix_missing_dialog_hyphen_3"] = ["- <i>test line 1</i>\n", "test line 2\n", "- test line 3\n"]
 
-TEST_LINES["fix_redundant_italic_tag"] = ["<i>test</i> <i>line 1</i><i></i>\n", "<i>test</i><i> line 2</i>\n"]
-RESULT_LINES["fix_redundant_italic_tag"] = ["<i>test line 1\n", "test line 2</i>\n"]
+    TEST_LINES["fix_double_quotes_errors_1"] = ["test line 1\n", "test line\"\n"]
+    RESULT_LINES["fix_double_quotes_errors_1"] = ["\"test line 1\n", "test line\"\n"]
+    TEST_LINES["fix_double_quotes_errors_2"] = ["test \"line 1\n", "test\" line\n"]
+    RESULT_LINES["fix_double_quotes_errors_2"] = ["test \"line 1\n", "test\" line\n"]
+    TEST_LINES["fix_double_quotes_errors_3"] = ["<i>test line 1\n", "test line\"</i>\n"]
+    RESULT_LINES["fix_double_quotes_errors_3"] = ["<i>\"test line 1\n", "test line\"</i>\n"]
+    TEST_LINES["fix_double_quotes_errors_4"] = ["test \"line 1\n", "test line\"\n"]
+    RESULT_LINES["fix_double_quotes_errors_4"] = ["test \"line 1\n", "test line\"\n"]
+    TEST_LINES["fix_double_quotes_errors_5"] = ["- test line 1\n", "- test line\"\n"]
+    RESULT_LINES["fix_double_quotes_errors_5"] = ["- test line 1\n", "- \"test line\"\n"]
+    TEST_LINES["fix_double_quotes_errors_6"] = ["- <i>test line 1\"</i>\n", "- test line\n"]
+    RESULT_LINES["fix_double_quotes_errors_6"] = ["- <i>\"test line 1\"</i>\n", "- test line\n"]
 
-TEST_LINES["fix_useless_dialog_hyphen_1"] = ["- test line\n"]
-RESULT_LINES["fix_useless_dialog_hyphen_1"] = ["test line\n"]
-TEST_LINES["fix_useless_dialog_hyphen_2"] = ["<i>- test line\n"]
-RESULT_LINES["fix_useless_dialog_hyphen_2"] = ["<i>test line\n"]
-TEST_LINES["fix_useless_dialog_hyphen_3"] = ["-  test line\n", "test line\n"]
-RESULT_LINES["fix_useless_dialog_hyphen_3"] = ["test line\n", "test line\n"]
-TEST_LINES["fix_useless_dialog_hyphen_4"] = ["<i>- test line\n", "test line\n"]
-RESULT_LINES["fix_useless_dialog_hyphen_4"] = ["<i>test line\n", "test line\n"]
-TEST_LINES["fix_useless_dialog_hyphen_5"] = ["\"- test line\n", "test line\n"]
-RESULT_LINES["fix_useless_dialog_hyphen_5"] = ["\"test line\n", "test line\n"]
-TEST_LINES["fix_useless_dialog_hyphen_6"] = ["\"<i>- test line\n", "test line\n", "test line\n"]
-RESULT_LINES["fix_useless_dialog_hyphen_6"] = ["\"<i>test line\n", "test line\n", "test line\n"]
+    TEST_LINES["fix_empty_lines_1"] = ["test line 1\n", "\n", "test line\n"]
+    RESULT_LINES["fix_empty_lines_1"] = ["test line 1\n", "test line\n"]
+    TEST_LINES["fix_empty_lines_2"] = ["test line 1\n", "  \n", "test line\n"]
+    RESULT_LINES["fix_empty_lines_2"] = ["test line 1\n", "test line\n"]
 
-TEST_LINES["fix_missing_dialog_hyphen_1"] = ["<i>test line 1</i>\n", "- test line 2\n"]
-RESULT_LINES["fix_missing_dialog_hyphen_1"] = ["- <i>test line 1</i>\n", "- test line 2\n"]
-TEST_LINES["fix_missing_dialog_hyphen_2"] = ["<i>test line 1</i>\n", "- test line 2\n", "- test line 3\n"]
-RESULT_LINES["fix_missing_dialog_hyphen_2"] = ["- <i>test line 1</i>\n", "- test line 2\n", "- test line 3\n"]
-TEST_LINES["fix_missing_dialog_hyphen_3"] = ["<i>test line 1</i>\n", "test line 2\n", "- test line 3\n"]
-RESULT_LINES["fix_missing_dialog_hyphen_3"] = ["- <i>test line 1</i>\n", "test line 2\n", "- test line 3\n"]
+    TEST_LINES["fix_sdh_tags_1"] = ["MAN ON RADIO : test line 1\n", "test line 2\n"]
+    RESULT_LINES["fix_sdh_tags_1"] = ["test line 1\n", "test line 2\n"]
+    TEST_LINES["fix_sdh_tags_2"] = ["- <i>MAN ON RADIO : test line 1\n", "test line 2</i>\n"]
+    RESULT_LINES["fix_sdh_tags_2"] = ["- <i>test line 1\n", "test line 2</i>\n"]
+    TEST_LINES["fix_sdh_tags_3"] = ["<i>MAN 1 : test line 1</i>\n", "- test line 2\n"]
+    RESULT_LINES["fix_sdh_tags_3"] = ["- <i>test line 1</i>\n", "- test line 2\n"]
+    TEST_LINES["fix_sdh_tags_4"] = ["[PLOP]\n"]
+    RESULT_LINES["fix_sdh_tags_4"] = [""]
 
-TEST_LINES["fix_double_quotes_errors_1"] = ["test line 1\n", "test line\"\n"]
-RESULT_LINES["fix_double_quotes_errors_1"] = ["\"test line 1\n", "test line\"\n"]
-TEST_LINES["fix_double_quotes_errors_2"] = ["test \"line 1\n", "test\" line\n"]
-RESULT_LINES["fix_double_quotes_errors_2"] = ["test \"line 1\n", "test\" line\n"]
-TEST_LINES["fix_double_quotes_errors_3"] = ["<i>test line 1\n", "test line\"</i>\n"]
-RESULT_LINES["fix_double_quotes_errors_3"] = ["<i>\"test line 1\n", "test line\"</i>\n"]
-TEST_LINES["fix_double_quotes_errors_4"] = ["test \"line 1\n", "test line\"\n"]
-RESULT_LINES["fix_double_quotes_errors_4"] = ["test \"line 1\n", "test line\"\n"]
-TEST_LINES["fix_double_quotes_errors_5"] = ["- test line 1\n", "- test line\"\n"]
-RESULT_LINES["fix_double_quotes_errors_5"] = ["- test line 1\n", "- \"test line\"\n"]
-TEST_LINES["fix_double_quotes_errors_6"] = ["- <i>test line 1\"</i>\n", "- test line\n"]
-RESULT_LINES["fix_double_quotes_errors_6"] = ["- <i>\"test line 1\"</i>\n", "- test line\n"]
 
-TEST_LINES["fix_empty_lines_1"] = ["test line 1\n", "\n", "test line\n"]
-RESULT_LINES["fix_empty_lines_1"] = ["test line 1\n", "test line\n"]
-TEST_LINES["fix_empty_lines_2"] = ["test line 1\n", "  \n", "test line\n"]
-RESULT_LINES["fix_empty_lines_2"] = ["test line 1\n", "test line\n"]
-
-TEST_LINES["fix_sdh_tags_1"] = ["MAN ON RADIO : test line 1\n", "test line 2\n"]
-RESULT_LINES["fix_sdh_tags_1"] = ["test line 1\n", "test line 2\n"]
-TEST_LINES["fix_sdh_tags_2"] = ["- <i>MAN ON RADIO : test line 1\n", "test line 2</i>\n"]
-RESULT_LINES["fix_sdh_tags_2"] = ["- <i>test line 1\n", "test line 2</i>\n"]
-TEST_LINES["fix_sdh_tags_3"] = ["<i>MAN 1 : test line 1</i>\n", "- test line 2\n"]
-RESULT_LINES["fix_sdh_tags_3"] = ["- <i>test line 1</i>\n", "- test line 2\n"]
-TEST_LINES["fix_sdh_tags_4"] = ["[PLOP]\n"]
-RESULT_LINES["fix_sdh_tags_4"] = [""]
-
-# endregion TEST_LINES Multi
+populate_single_line_test_dict()
+populate_multi_line_test_dict()
 
 
 class TestStringsUtils(unittest.TestCase):
-
-    # region Utils
-
-    def assert_list_equals_test(self, corrected_lines, key, tag):
-        if tag in key:
-            self.assertListEqual(corrected_lines, RESULT_LINES[key])
-        else:
-            self.assertListEqual(corrected_lines, TEST_LINES[key])
-
-    # endregion Utils
 
     # region Single-line
 
@@ -132,7 +124,14 @@ class TestStringsUtils(unittest.TestCase):
     # TODO : test_fix_fix_dialog_hyphen
     # TODO : test_fix_letter_followed_by_space
     # TODO : test_fix_italic_tag_errors
-    # TODO : test_fix_colon
+
+    def test_fix_colon(self):
+        for key in TEST_LINES:
+            corrected_line = []
+            for i in range(0, len(TEST_LINES[key])):
+                corrected_line.append(StringsUtils.fix_colon(TEST_LINES[key][i]))
+
+            self.assert_list_equals_test(corrected_line, key, "fix_colon")
 
     def test_fix_common_misspells(self):
         for key in TEST_LINES:
@@ -149,7 +148,7 @@ class TestStringsUtils(unittest.TestCase):
         for key in TEST_LINES:
             corrected_line = []
             for i in range(0, len(TEST_LINES[key])):
-                corrected_line.append(StringsUtils.fix_numbers(TEST_LINES[key][i], unittest_data=FIX_NUMBERS_PROMPTS))
+                corrected_line.append(StringsUtils.fix_numbers(TEST_LINES[key][i], unittest_data=RESULT_PROMPT["fix_numbers"]))
 
             self.assert_list_equals_test(corrected_line, key, "fix_numbers")
 
@@ -185,6 +184,8 @@ class TestStringsUtils(unittest.TestCase):
 
             self.assert_list_equals_test(corrected_line, key, "fix_acronyms")
 
+    # endregion Single-line
+
     # region Multi-line
 
     def test_fix_empty_lines(self):
@@ -216,6 +217,14 @@ class TestStringsUtils(unittest.TestCase):
         for key in TEST_LINES:
             corrected_lines = StringsUtils.fix_sdh_tags(TEST_LINES[key])
             self.assert_list_equals_test(corrected_lines, key, "fix_sdh_tags")
+
+    # endregion Multi-line
+
+    def assert_list_equals_test(self, corrected_lines, key, tag):
+        if tag in key:
+            self.assertListEqual(corrected_lines, RESULT_LINES[key])
+        else:
+            self.assertListEqual(corrected_lines, TEST_LINES[key])
 
 
 if __name__ == '__main__':
