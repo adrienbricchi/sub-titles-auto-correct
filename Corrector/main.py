37f2e08dc96925112db88aa3fa875609ff461a9b
Adrien Bricchi <adrienbricchi@users.noreply.github.com>
2017-01-18 22:26:26 +0100

    Version bump

--- Corrector/main.py
+++ Corrector/main.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from tkinter import Menu, Listbox, END, Label, Tk           # GUI, needs python3-tk package on Linux
 import locale                                               # get current system language
